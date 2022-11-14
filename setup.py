163de9ac73daf47bffe3bc47add32bb6ba896df6
Kaahan <kaahan@neohuman.life>
2022-11-14 18:37:55 +0000

    remove pycache, add inits, fix setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="insight-face-rest",
     version="0.7.0.0",
-    packages = find_packages(exclude=[])
+    packages=find_packages(exclude=[]),
 )
